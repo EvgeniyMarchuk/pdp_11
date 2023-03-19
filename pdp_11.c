eae3267ec39bdb25241a8cc748562f9288b1af90
Марчук Евгений <marchuk.ep@phystech.edu>
2023-03-19 13:43:39 +0300

    load_data + mem_dump

--- pdp_11.c
+++ pdp_11.c
@@ -15,11 +15,16 @@ byte b_read (address adr);
 void w_write (address adr, word val);
 word w_read (address adr);
 
+void load_data();
+void mem_dump(address adr, int size);
+
 void test_mem();
 
 int main()
 {	
-	test_mem();
+	//test_mem();
+	
+	load_data();
 	
     return 0;
 }
@@ -41,7 +46,10 @@ void b_write(address adr, byte val)
 	if ((adr & 1) == 0)
 		mem[adr] = (word)val;
 	else
+	{	
 		mem[adr-1] |= (((word)val) << 8);
+		//printf("\n%0x \n", mem[adr-1]);
+	}
 }
 
 void w_write(address adr, word val)
@@ -54,6 +62,36 @@ word w_read(address adr)
 {
 	assert((adr & 1) == 0);
 	return mem[adr];
+}
+
+void load_data()
+{
+	address adr_in_mem = 0;
+	unsigned char N = 0;
+	
+	while (fscanf(stdin, "%hx %hhx", &adr_in_mem, &N) == 2)  
+	{
+		for (unsigned char i = 0; i < N; i++)
+		{
+			byte num;
+			scanf("%hhx", &num);
+			
+			b_write(adr_in_mem, num);
+			
+			adr_in_mem++;
+		}
+	}
+}
+
+void mem_dump(address adr, int size)
+{
+	for (unsigned int i = 0; i < size/2; i++)
+	{
+		word res = w_read(adr);
+		
+		printf("%06o: %06o %04x", adr, res, res);
+		adr += 2;
+	}
 }
 
 void test_mem()
@@ -91,7 +129,7 @@ void test_mem()
     assert(w == wres);
 	
 	
-	
+	// работа с нечетным адресом 
     // пишем 2 байта, читаем 1 слово
     fprintf(stderr, "Пишем 2 байта, читаем слово\n");
     a = 3;        // другой адрес
@@ -99,13 +137,12 @@ void test_mem()
     // little-endian, младшие разряды по меньшему адресу
     b0 = 0xb2;
     b1 = 0xa1;
-    b_write(a, b0);
-    b_write(a+1, b1);
-    wres = w_read(a);
+    b_write(a-1, b0);
+	//wres = w_read(a-1);
+	//printf("\n%d ||||||||", wres);
+    b_write(a, b1);
+    wres = w_read(a-1);
     // тут полезно написать отладочную печать a, w, wres
     fprintf(stderr, "a=%06o b1=%02hhx b0=%02hhx wres=%04x \n", a, b1, b0, wres);
     assert(w == wres);
-	
-	
-    // еще тесты
 }
