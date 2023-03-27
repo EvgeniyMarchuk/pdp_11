24dd6ac207c45a953f0e1b06c704833e33aa1fc8
Марчук Евгений <marchuk.ep@phystech.edu>
2023-03-27 17:21:33 +0300

    add function Log

--- pdp_11.c
+++ pdp_11.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
 #define MEMSIZE (64*1024)
+#define SIZE 100
 
 typedef unsigned char byte;
 typedef unsigned short int word;
 typedef word address;
+
+enum level{
+	DEBUG,
+	TRACE,
+	INFO,
+	ERROR
+};
+
+unsigned int log_level = INFO;
 
 word mem[MEMSIZE];
 
@@ -21,22 +33,34 @@ void mem_dump(address adr, unsigned int size);
 
 void load_file(char *name_file);
 
+void Log(unsigned int level, char *str, ...);
+unsigned int set_log_level(unsigned int level);
+
 void test_mem();
+void test_file();
+
+
+
+
 
 int main(int argc, char *argv[])
 {	
 	if (argc == 1)
 	{
-		printf("Data will read from console\n");
+		Log(log_level, "Data will read from console, if you want read from file, please, write -t after ./a.out\n");
 		load_data();
 	}
 	else if (strcmp(argv[1],"-t") == 0)
 		load_file(argv[argc-1]);
 	else
-		printf("Incorrect program launch");
+		Log(log_level, "Incorrect program launch");
 	
     return 0;
 }
+
+
+
+
 
 byte b_read(address adr)
 {
@@ -55,10 +79,7 @@ void b_write(address adr, byte val)
 	if ((adr & 1) == 0)
 		mem[adr] = (word)val;
 	else
-	{	
 		mem[adr-1] |= (((word)val) << 8);
-		//printf("\n%0x \n", mem[adr-1]);
-	}
 }
 
 void w_write(address adr, word val)
@@ -72,6 +93,10 @@ word w_read(address adr)
 	assert((adr & 1) == 0);
 	return mem[adr];
 }
+
+
+
+
 
 void load_data()
 {
@@ -98,7 +123,7 @@ void mem_dump(address adr, unsigned int size)
 	{
 		word res = w_read(adr);
 		
-		printf("%06o: %06o %04x\n", adr, res, res);
+		Log(log_level, "%06o: %06o %04x\n", adr, res, res);
 		adr += 2;
 	}
 }
@@ -108,8 +133,10 @@ void load_file(char *name_file)
 	FILE *data_file = fopen(name_file, "r");
 	
 	if (data_file == NULL)
+	{
 		perror("Ошибка открытия файла");
-	
+		exit(1);
+	}
 	address adr_in_mem = 0;
 	unsigned char N = 0;
 	
@@ -128,6 +155,36 @@ void load_file(char *name_file)
 	
 	fclose(data_file);
 }
+
+
+
+
+
+void Log(unsigned int level, char *str, ...)
+{
+	if (level >= log_level)
+	{
+		va_list ap;
+		va_start(ap, str);
+		
+		vprintf(str, ap);	
+		
+		va_end(ap);
+	}
+}
+
+unsigned int set_log_level(unsigned int level)
+{
+	unsigned int old_log = log_level;
+	
+	log_level = level;
+	
+	return old_log;
+}
+
+
+
+
 
 void test_mem()
 {
@@ -182,7 +239,7 @@ void test_mem()
     assert(w == wres);
 }
 
-/*void test_load(char *name_file)
+void test_file()
 {
-	;
-}*/
+	mem_dump(66, 2);
+}
